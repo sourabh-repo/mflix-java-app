26acc4b5206743b0666090e4fe0db048a0d47544
Sourabh Satapathy <Sourabh.Satapathy2@cerner.com>
2020-04-22 20:22:46 -0400

    init

---
+++ src/main/java/mflix/api/models/Session.java
@@ -0,0 +1,32 @@
+package mflix.api.models;
+
+import org.bson.codecs.pojo.annotations.BsonProperty;
+import org.bson.types.ObjectId;
+
+public class Session {
+
+    @BsonProperty(value = "user_id")
+    private String userId;
+
+    private String jwt;
+
+    public Session() {
+        super();
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getJwt() {
+        return jwt;
+    }
+
+    public void setJwt(String jwt) {
+        this.jwt = jwt;
+    }
+}
